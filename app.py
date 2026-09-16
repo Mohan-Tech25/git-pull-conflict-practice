fcefd4757222b631a40a05672346a6b5007397d7
Mohan Munusamy <mohanmohanmunusamy7@gmail.com>
2026-09-16 14:40:06 +0530

    Change greeting in VS Code branch

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 def greet():
-    print("Hello from main")
+    print("Hello from VS Code branch")
     print("Hello from Developer A")
     print("Hello from unfinished work")
     print("Change from feature-test branch")
