541df3748d3f9c96bb51b89c39f1a62bfe399b59
Mohan Munusamy <mohanmohanmunusamy7@gmail.com>
2026-09-16 16:43:16 +0530

    update main

--- app.py
+++ app.py
@@ -5,14 +5,11 @@ def greet():
     print("Hello from unfinished work")
     print("Change from feature-test branch resolve using vscode editor")
     print("Change from feature-test branch manually added")
-
-    print(" Switching from feature-test to main branch and adding a new line")
-
-    print( "pulled the main branch from repo and added a new line to the main branch then push to the repo")
-
-
+    print("Switching from feature-test to main branch and adding a new line")
+    print("Pulled the main branch from repo and added a new line to the main branch then push to the repo")
     print("New change from conflict-test")
+    print("Hello from pull request branch")
+    print("Created one new branch to check the pull request feature of GitHub")
 
-    print ("helloe form pull request branch")
 
-    print(" created one new branch to check the pull request feature of github")
+greet()
